1546ddf2ab7469700b54d300f950a555cf94dcf7
Taylor Damann <taydamann@yahoo.com>
2020-02-26 11:41:14 -0600

    Initial Commit
    Created a shell of the problem set

---
+++ PDS PS3.R
@@ -0,0 +1,13 @@
+###TAYLOR DAMANN###
+#####PDS PS3#####
+
+########ggplot#########
+#1- finish what we started in class 2/11/20
+
+#2- finish what we started on 2/13/2020
+
+######tidyverse######
+#3
+
+########text as data#######
+#4
